79583dd95eadb3cc1e46b12cca1ccfa6f7002a77
Kshitiz <kshitizjain2020@gmail.com>
2026-04-14 12:00:12 +0530

    Implemented car horizontal scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,31 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
     
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother} from "gsap/ScrollSmoother";
 
 const App = () => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    useGSAP(()=>{
+      gsap.to('.car,.path',{
+        x:'101.5vw',
+        scrollTrigger:{
+          trigger: ".section",
+          start: "top top",
+          end:'bottom top',
+          markers:true,
+          scrub:true
+        }
+      })
+    })
   return (
-    <div className='section'>
-      <div className="track h-screen flex justify-center items-center">
-        <div className="road relative h-50 w-full bg-[#1e1e1e] overflow-hidden flex items-center">
-          {/* <img className='car h-45 absolute' src="/McLaren.png" alt="" /> */}
-          <div className="trail flex justify-center items-center h-full w-full bg-[#45db7d]">
-            <div className="text text-[8rem] tracking-wider font-bold">WELCOME ITZFIZZ</div>
+    <div className='section h-[300vh]'>
+      <div className="track h-screen sticky top-0 flex justify-center items-center">
+        <div className="road relative h-50 w-full  overflow-hidden flex items-center">
+          <img className='car z-2 h-50 absolute' src="/McLaren.png" alt="" />
+          <div className="path bg-[#1e1e1e] h-full w-full z-1 ml-19 absolute top-0"></div>
+          <div className="trail  flex justify-center items-center h-full w-full bg-[#45db7d]">
+            <div className="text mr-25 text-[8rem] tracking-wider font-bold">WELCOME ITZFIZZ</div>
           </div>
         </div>
 
